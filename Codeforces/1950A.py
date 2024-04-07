03106d3981ba81782d5a49aaf4b217a151d0c7e9
Tapantor Das <109220196+Cpsbd66@users.noreply.github.com>
2024-04-07 12:44:29 +0600

    Create 1950A.py

---
+++ Codeforces/1950A.py
@@ -0,0 +1,6 @@
+test = int(input())
+for t in range(test):
+    a, b, c = map(int,input().split(' '))
+    if(a<b and b<c):print("STAIR")
+    elif(a<b and b>c):print("PEAK")
+    else:print("NONE")
